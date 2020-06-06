3c9c3bd9b2d079a5bf826be2393c29b6e7d87985
Catheryne Small <catheryn.small@gmail.com>
2020-06-06 15:17:07 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,10 @@
 class Backer
   attr_accessor :backed_projects
-  attr_reader :name 
+  attr_reader :name, :title 
   
-  def initialize(name)
+  def initialize(name, title)
     @name = name
     @backed_projects = []
+    @title = title
   end
 end
